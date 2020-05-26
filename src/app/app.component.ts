81ac9915bdec775419acb5013c1f12edd1ec5613
Ratnesh Kumar Singh <ratnesh.kumar@harappa.education>
2020-05-26 22:21:50 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'May26';
+  title = 'https://fakeimg.pl/250x100/ff0000/';
+
+  bgColor = 'red';
+
+  columns = 2;
+
+  active = false;
+
+  items = ['bmw', 'maruti', 'ferrari'];
+
+  constructor() {
+    console.log('constriuctor')
+  }
+
+  toggleActive() {
+    this.active = !this.active;
+  }
+
+
+
+  ngOnInit() {
+    for (let item of this.items) {
+      console.log(item);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent
   ],
   imports: [
     BrowserModule,
