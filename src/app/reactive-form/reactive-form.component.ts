0f64c60add69d5f5c46f9f5667924630fc100134
ratnesh singh <dev.ratnesh.singh@gmail.com>
2020-06-06 21:58:15 +0530

    form array and grouping

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -11,23 +11,65 @@ export class ReactiveFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.minLength(8), Validators.required])
-    });
+    // this.form = new FormGroup({
+    //   email: new FormControl('', [Validators.required, Validators.email]),
+    //   password: new FormControl('', [Validators.minLength(8), Validators.required])
+    // });
+    this.form = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required, Validators.minLength(8)]),
+      address: this.fb.group({
+        city: this.fb.control('', [Validators.required]),
+        country: this.fb.control('', [Validators.required])
+      }),
+      companies: this.fb.array([])
+    })
   }
 
   submit() {
+    console.log(this.form.value);
   }
 
-  get password(){
+  get password() {
     return this.form.get('password');
   }
 
-  get email(){
+  get email() {
     return this.form.get('email');
   }
+
+  get city() {
+    return this.form.get('address').get('city');
+  }
+
+  get country() {
+    return this.form.get('address').get('country');
+  }
+
+  get companies() {
+    return (this.form.get('companies') as FormArray).controls;
+  }
+
+  addCompany() {
+    (this.form.get('companies') as FormArray).push(this.createCompany());
+  }
+
+  createCompany() {
+    return this.fb.group({
+      companyName: this.fb.control('', [Validators.required])
+    })
+  }
+
+  removeCompany(index) {
+    (this.form.get('companies') as FormArray).removeAt(index);
+  }
 }
+
+
+// {
+//   email: 'asdfghjkl'
+//   password: 'sdfghjkl'
+// }
